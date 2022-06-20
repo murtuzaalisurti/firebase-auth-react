5e7a5ee9e984c686a402905532093d89048c8934
murtuza <murtuza.creativity@gmail.com>
2022-06-20 15:28:40 +0530

    authentication done ✔

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { Route, Routes, BrowserRouter } from 'react-router-dom'
 import Home from "./pages/Home";
 import LoginAdmin from "./pages/LoginAdmin";
 import ProtectedRoute from "./components/ProtectedRoute";
-import Questions from "./pages/Questions";
 
 function App() {
   return (
@@ -12,7 +11,6 @@ function App() {
         <Routes>
           <Route element={<ProtectedRoute />}>
             <Route path="/" element={<Home />} />
-            <Route path="/newform" element={<Questions />} />
           </Route>
           <Route path="/registerAdmin" element={<RegisterAdmin />} />
           <Route path="/loginAdmin" element={<LoginAdmin />} />
